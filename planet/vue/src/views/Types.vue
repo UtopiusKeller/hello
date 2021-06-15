5a8efb6e133a807dc5f62331bf01965744d645bb
UtopiusKeller <utopius777@ShadowDesktop.localdomain>
2021-06-15 09:16:15 -0400

    mods

---
+++ planet/vue/src/views/Types.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<div class="container">
+			<!-- this line is used by starport scaffolding # 4 -->
+		<SpType modulePath="hello.planet.blog" moduleType="TimedoutPost"  />
+		<SpType modulePath="hello.planet.blog" moduleType="SentPost"  />
+		<SpType modulePath="hello.planet.blog" moduleType="Post"  />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Types'
+}
+</script>
